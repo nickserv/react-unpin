19f534afa68bb811fe251ca650dfa0304aaedc44
Nick McCurdy <nick@nickmccurdy.com>
2024-01-05 15:31:06 -0500

    Implement initial integration tests

---
+++ test.js
@@ -0,0 +1,15 @@
+import assert from "assert/strict";
+import test from "node:test";
+import reactVersion, { nextManifest, nextVersion } from "./index.js";
+
+test("nextVersion", async () => {
+	assert.equal(await nextVersion(), "14.0.4");
+});
+
+test("nextManifest", async () => {
+	assert.notDeepEqual(await nextManifest(), {});
+});
+
+test("reactVersion", async () => {
+	assert.match(await reactVersion(), /^18.3.0-canary-/);
+});
