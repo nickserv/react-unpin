d75056047af5223d4e2ac9cd290e99b631a98330
Nick McCurdy <nick@nickmccurdy.com>
2024-05-25 06:18:34 -0400

    Test with Next 15 and React 19

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import test from "node:test";
 import reactVersion, { nextManifest, nextVersion } from "./index.js";
 
 test("nextVersion", async () => {
-	assert.equal(await nextVersion(), "14.0.4");
+	assert.equal(await nextVersion(), "15.0.0-rc.0");
 });
 
 test("nextManifest", async () => {
@@ -11,5 +11,5 @@ test("nextManifest", async () => {
 });
 
 test("reactVersion", async () => {
-	assert.match(await reactVersion(), /^18\.3\.0-canary-/);
+	assert.match(await reactVersion(), /^19\.0\.0-rc-/);
 });
